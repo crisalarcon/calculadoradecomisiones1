75dc7e2570342ca9b120bd5b2e0fa23f0b478986
crisalarcon <cris.marin.alarcon@gmail.com>
2023-12-14 00:09:01 -0300

    init commit

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+# Calculadora de comisiones
 
+nombre = input("Por favor, introduce tu nombre: ")
+ventas = int(input("introduce tus ventas totales del mes: "))
+
+comision = round(ventas * 13 / 100, 2)
+
+print(f"{nombre}, tus comisiones de este mes son de ${comision}")
